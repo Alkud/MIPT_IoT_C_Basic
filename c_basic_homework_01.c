9d88bc7bacb4072e0cad368524ad7a18ccacc1c2
Alexander Kudinov <alexandr_kudinov@mail.ru>
2024-02-24 19:34:05 +0300

    Added Basic C HW#1

---
+++ c_basic_homework_01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    /* 1. Hello world! */
+    printf("Hello world!\n\n");
+    /* 2. Tabulated text */
+    printf("Let's\n");
+    printf("\tgo\n");
+    printf("\t\tto walk!\n\n");
+    /* 3. ASCII art*/
+    printf("   *   \n");
+    printf("  ***  \n");
+    printf(" ***** \n");
+    printf("*******\n");
+    printf(" HH HH \n");
+    printf(" ZZZZZ \n");
+
+    return 0;
+}
